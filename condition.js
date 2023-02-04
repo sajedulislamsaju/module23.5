f48b0b77f8ba1eb0979b0705c8463be97ec8fc4d
sajedulislamsaju <sajedulislamsaju123@gmail.com>
2023-02-04 23:02:54 +0600

    the condition is added

---
+++ condition.js
@@ -0,0 +1,9 @@
+var iphoone = 79000;
+var myBazet = 25000;
+
+if (iphoone < myBazet){
+    console.log('Iphone kine futani marbo hm!!')
+}
+else{
+    console.log('xiaomi brand er kunu ekta kine felbo ok')
+}

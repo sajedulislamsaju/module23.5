c4c2f8ebe32a0bd24f90db4927b1b382f9452ba3
sajedulislamsaju <sajedulislamsaju123@gmail.com>
2023-02-04 23:18:02 +0600

    the multipul condition id add

---
+++ multipul-condition.js
@@ -0,0 +1,10 @@
+var grajuat = true;
+var selary = 65000;
+var car = 1;
+
+if (grajuat == true && selary > 50000){
+    console.log("aso bia kore feli ")
+}
+else{
+    console.log('tumar friend er number deu ')
+}

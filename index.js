294ad85ff2741ad73c29e0652ea6bcc3dc86ba0d
sajedulislamsaju <sajedulislamsaju123@gmail.com>
2023-02-04 23:43:40 +0600

    the index system is add

---
+++ index.js
@@ -0,0 +1,13 @@
+var arraye = ["managram", 'katiadi', 'kishoreganj', 'pulergat', 'dasergaw']
+
+console.log(arraye[2] = 'munsigonj')
+
+arraye[2] = "komolpur"; 
+
+var index =arraye.indexOf('pulergat')
+
+ console.log(index)
+
+arraye.push('dhaka')
+
+console.log(arraye)

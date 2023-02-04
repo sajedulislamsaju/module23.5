a0a450134fcbfbbe06de6b463a3a6e9c18e72d77
sajedulislamsaju <sajedulislamsaju123@gmail.com>
2023-02-04 15:33:04 +0600

    first commit

---
+++ stringNumber.js
@@ -0,0 +1,23 @@
+// this is the string Number and some parseInt system .
+
+var age = "20";
+var friend = "20";
+const total = age + friend;
+const ageNumber = parseInt (age)
+const friendNumber = parseInt (friend)
+// console.log(ageNumber)
+// console.log(friendNumber)
+// console.log(ageNumber+friendNumber)
+// console.log(total)
+
+// I want to know that is what type of variable is 
+
+
+
+const toffee = 29;
+
+console.log(typeof toffee)
+
+
+
+
